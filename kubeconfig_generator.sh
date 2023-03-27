71f994369003fcc409b06c450657470fd455dfe7
Atul Chauhan <atulchauhan.01@gmail.com>
2023-03-27 09:13:35 +0530

    Initial commit - adding files for multiple tools

---
+++ kubeconfig_generator.sh
@@ -0,0 +1,30 @@
+# https://stackoverflow.com/questions/47770676/how-to-create-a-kubectl-config-file-for-serviceaccount
+# your server name goes here
+server=https://c112.us-south.containers.cloud.ibm.com:31276
+# the name of the secret containing the service account token goes here (specific to namespace)
+name=jenkins-token-lhklx
+
+ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
+namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: inventory-mgmt-np-dal/c154ol3d0en954gn63gg
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: jenkins-default-context
+  context:
+    cluster: inventory-mgmt-np-dal/c154ol3d0en954gn63gg
+    namespace: default
+    user: jenkins-token-lhklx
+current-context: jenkins-default-context
+users:
+- name: jenkins-token-lhklx
+  user:
+    token: ${token}
+" > np-dal.kubeconfig
